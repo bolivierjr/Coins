d1a295df953bc4f0319011f27987261c47ad1649
Bruce Olivier <bolivierjr@gmail.com>
2017-12-12 19:28:15 -0600

    moving repository down into Coins/, woops

---
+++ test.py
@@ -0,0 +1,15 @@
+###
+# Copyright (c) 2017, Bruce Olivier
+# All rights reserved.
+#
+#
+###
+
+from supybot.test import *
+
+
+class CoinsTestCase(PluginTestCase):
+    plugins = ('Coins',)
+
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
